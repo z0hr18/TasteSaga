d5c44bdadfa6ee3df6dfa358fc9352469f3ad3f7
Zohra <guliyevaazohra@gmail.com>
2024-01-11 15:33:58 +0400

    create file

---
+++ Taste Saga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taste Saga
+//
+//  Created by Zohra Guliyeva on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
