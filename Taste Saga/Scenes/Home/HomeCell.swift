32e32adb92bf918ff57b49f4290c338a9de639c5
Zohra <guliyevaazohra@gmail.com>
2024-02-16 16:34:29 +0400

    commit

--- Taste Saga/Scenes/Home/HomeCell.swift
+++ Taste Saga/Scenes/Home/HomeCell.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class HomeCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var foodİmage: UICollectionView!
+    
+    @IBOutlet weak var foodTitleLabel: UILabel!
+    @IBOutlet weak var foodSubtitleLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var serviceLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+       
     }
 
 }
--- Taste Saga/Scenes/Home/HomeController.swift
+++ Taste Saga/Scenes/Home/HomeController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    @IBOutlet weak var colllectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+       
+        self.colllectionView.register(UINib(nibName: "HomeCell", bundle: nil), forCellWithReuseIdentifier: "HomeCell")
+                   
     }
+}
+
+extension HomeController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        5
+    }
+    
+    
 }
